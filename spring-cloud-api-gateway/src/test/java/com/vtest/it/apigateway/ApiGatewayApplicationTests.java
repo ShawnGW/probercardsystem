0717f6ad3afa9701dc63d653681d7c5e59e31eb9
shawn <Mailsunguowei@163.com>
2019-10-17 13:20:38 +0800

    add api gateway

---
+++ spring-cloud-api-gateway/src/test/java/com/vtest/it/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtest.it.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
