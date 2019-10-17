ff9d1d2d351faadb1a6af143db73a1beecdbc5dd
shawn <Mailsunguowei@163.com>
2019-10-17 17:34:57 +0800

    add new module tooling  service

---
+++ spring-cloud-tooling-service/src/test/java/com/vtest/it/springcloudtoolingservice/SpringCloudToolingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtest.it.springcloudtoolingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudToolingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
