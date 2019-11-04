6fe67f58b015ade016344ab9b57ef5c17041dfa3
shawn <Mailsunguowei@163.com>
2019-11-04 14:51:14 +0800

    add prober card  service

---
+++ spring-cloud-auth-and-operator/src/test/java/com/vtest/it/springcloudauthandoperator/SpringCloudAuthAndOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtest.it.springcloudauthandoperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAuthAndOperatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
